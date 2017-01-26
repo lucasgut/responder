8a71d9c358a0b7705bb0ef2b56b3c199b878bf59
Andres C. Burgos <aburgos@temenos.com>
2017-01-26 12:07:26 +0000

    Added transaction date to T24 standing orders mock

--- commands/src/main/groovy/com/temenos/responder/commands/dashboard/T24StandingOrder.groovy
+++ commands/src/main/groovy/com/temenos/responder/commands/dashboard/T24StandingOrder.groovy
@@ -28,18 +28,22 @@ class T24StandingOrder implements Command {
                 map.put("ID", 200)
                 map.put("TARGET.ACCOUNT", "GB91 BKEN 1000 0041 6100 08")
                 map.put("AMOUNT", 1200.0)
+                map.put("TRANSACTION.DATE", "2001-01-26 11:58:29")
             } else if(standingOrderId == "400") {
                 map.put("ID", 400)
                 map.put("TARGET.ACCOUNT", "GB27 BOFI 9021 2729 8235 29")
                 map.put("AMOUNT", 2020.0)
+                map.put("TRANSACTION.DATE", "2008-05-14 16:02:50")
             } else if(standingOrderId == "401") {
                 map.put("ID", 401)
                 map.put("TARGET.ACCOUNT", "GB29 NWBK 6016 1331 9268 19")
                 map.put("AMOUNT", 2000.0)
+                map.put("TRANSACTION.DATE", "2011-07-30 12:56:23")
             } else if(standingOrderId == "402") {
                 map.put("ID", 402)
                 map.put("TARGET.ACCOUNT", "GB29 NWBK 6016 1331 9268 53")
                 map.put("AMOUNT", 4000.0)
+                map.put("TRANSACTION.DATE", "1995-11-26 15:20:52")
             }
 
             context.setResponseCode(Response.Status.OK.statusCode as String)
--- commands/src/test/groovy/com/temenos/responder/commands/dashboard/T24StandingOrderTest.groovy
+++ commands/src/test/groovy/com/temenos/responder/commands/dashboard/T24StandingOrderTest.groovy
@@ -24,10 +24,10 @@ class T24StandingOrderTest extends Specification {
             1 * context.setResponseCode('200')
         where:
             id   | map
-            200  | ['ID': 200, 'TARGET.ACCOUNT': 'GB91 BKEN 1000 0041 6100 08', 'AMOUNT': 1200.0]
-            400  | ['ID': 400, 'TARGET.ACCOUNT': 'GB27 BOFI 9021 2729 8235 29', 'AMOUNT': 2020.0]
-            401  | ['ID': 401, 'TARGET.ACCOUNT': 'GB29 NWBK 6016 1331 9268 19', 'AMOUNT': 2000.0]
-            402  | ['ID': 402, 'TARGET.ACCOUNT': 'GB29 NWBK 6016 1331 9268 53', 'AMOUNT': 4000.0]
+            200  | ['ID': 200, 'TARGET.ACCOUNT': 'GB91 BKEN 1000 0041 6100 08', 'AMOUNT': 1200.0, 'TRANSACTION.DATE': '2001-01-26 11:58:29']
+            400  | ['ID': 400, 'TARGET.ACCOUNT': 'GB27 BOFI 9021 2729 8235 29', 'AMOUNT': 2020.0, 'TRANSACTION.DATE': '2008-05-14 16:02:50']
+            401  | ['ID': 401, 'TARGET.ACCOUNT': 'GB29 NWBK 6016 1331 9268 19', 'AMOUNT': 2000.0, 'TRANSACTION.DATE': '2011-07-30 12:56:23']
+            402  | ['ID': 402, 'TARGET.ACCOUNT': 'GB29 NWBK 6016 1331 9268 53', 'AMOUNT': 4000.0, 'TRANSACTION.DATE': '1995-11-26 15:20:52']
     }
 
     @Unroll
