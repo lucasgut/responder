3c7302031c1119b2e08de06885f470aea471a550
Douglas Groves <douglas.groves@ntlworld.com>
2017-01-27 15:33:54 +0000

    Fix README.MD and ResourceHandler feature names
    This commit updates the contents of README.MD and improves the readability
    of Spock features in ResourceHandlerTest.

--- src/test/groovy/com/temenos/responder/controller/ResourceHandlerTest.groovy
+++ src/test/groovy/com/temenos/responder/controller/ResourceHandlerTest.groovy
@@ -8,13 +8,16 @@ import com.temenos.responder.exception.MethodNotFoundException
 import com.temenos.responder.exception.VersionNotFoundException
 import com.temenos.responder.mapper.ResourceBuilder
 import spock.lang.Specification
+import spock.lang.Unroll
 
 /**
  * Created by aburgos on 13/01/2017.
  */
 class ResourceHandlerTest extends Specification {
 
-    def "Fetch #methodName version #versionName "(methodName, versionName, directives) {
+    @Unroll
+    def "Fetch model for method '#methodName' and \
+        model for version '#versionName'"(methodName, versionName, directives) {
         given:
             def parser = new ResourceBuilder()
             def nodeFactory = JsonNodeFactory.instance;
@@ -42,31 +45,9 @@ class ResourceHandlerTest extends Specification {
             "POST"     | "2.0"       | ["GET": ["routeTo": ["1.0": ["flow": "com.temenos.responder.flows.AdditionFlow", "response": ["description": "Account Dashboard response", "item": "savingsAPI.accounts/AccountDashboard"]], "2.0": ["flow": "com.temenos.responder.flows.VersionInformationFlow"]]], "POST": ["routeTo": ["1.0": ["flow": "com.temenos.responder.flows.AdditionFlow"], "2.0": ["flow": "com.temenos.responder.flows.VersionInformationFlow", "request": ["description": "Account Dashboard request", "item": "savingsAPI.accounts/AccountDashboard"]]]]]
     }
 
-    def "Getting nonexistent method"(methodName, versionName, directives) {
-        given:
-            def parser = new ResourceBuilder()
-            def nodeFactory = JsonNodeFactory.instance;
-            def childNode = nodeFactory.objectNode()
-            childNode.put(ResourceSpec.PATH, "/path")
-            ObjectMapper mapper = new ObjectMapper()
-            JsonNode methodsNode = mapper.valueToTree(directives)
-            childNode.set(ResourceSpec.DIRECTIVES, methodsNode)
-            def node = nodeFactory.objectNode()
-            node.set("anonymous", childNode)
-            def resource = parser.getResource(node)
-            def handler = new ResourceHandler()
-        when:
-            handler.getMethod(resource, methodName)
-        then:
-            thrown(MethodNotFoundException)
-        where:
-            methodName | versionName | directives
-            "POST"     | "default"   | ["GET": ["routeTo": ["default": ["flow": "com.temenos.responder.flows.AdditionFlow"]]]]
-            "DELETE"   | "2.0"       | ["GET": ["routeTo": ["1.0": ["flow": "com.temenos.responder.flows.AdditionFlow"], "2.0": ["flow": "com.temenos.responder.flows.VersionInformationFlow"]]], "POST": ["routeTo": ["1.0": ["flow": "com.temenos.responder.flows.AdditionFlow"], "2.0": ["flow": "com.temenos.responder.flows.VersionInformationFlow"]]]]
-            "PATCH"    | "2.0"       | ["GET": ["routeTo": ["1.0": ["flow": "com.temenos.responder.flows.AdditionFlow", "response": ["description": "Account Dashboard response", "item": "savingsAPI.accounts/AccountDashboard"]], "2.0": ["flow": "com.temenos.responder.flows.VersionInformationFlow"]]], "POST": ["routeTo": ["1.0": ["flow": "com.temenos.responder.flows.AdditionFlow"], "2.0": ["flow": "com.temenos.responder.flows.VersionInformationFlow", "request": ["description": "Account Dashboard request", "item": "savingsAPI.accounts/AccountDashboard"]]]]]
-    }
-
-    def "Getting nonexistent version"(methodName, versionName, directives) {
+    @Unroll
+    def "Throw #exception.simpleName if #description doesn't \
+        exist in the chosen resource definition"(exception, description, methodName, versionName, directives) {
         given:
             def parser = new ResourceBuilder()
             def nodeFactory = JsonNodeFactory.instance;
@@ -83,11 +64,14 @@ class ResourceHandlerTest extends Specification {
             handler.getMethod(resource, methodName)
             handler.getVersion(resource, methodName, versionName)
         then:
-            thrown(VersionNotFoundException)
+            thrown(exception)
         where:
-            methodName | versionName | directives
-            "GET"      | "1.0"       | ["GET": ["routeTo": ["default": ["flow": "com.temenos.responder.flows.AdditionFlow"]]]]
-            "GET"      | "3.0"       | ["GET": ["routeTo": ["1.0": ["flow": "com.temenos.responder.flows.AdditionFlow"], "2.0": ["flow": "com.temenos.responder.flows.VersionInformationFlow"]]], "POST": ["routeTo": ["1.0": ["flow": "com.temenos.responder.flows.AdditionFlow"], "2.0": ["flow": "com.temenos.responder.flows.VersionInformationFlow"]]]]
-            "POST"     | "4.0"       | ["GET": ["routeTo": ["1.0": ["flow": "com.temenos.responder.flows.AdditionFlow", "response": ["description": "Account Dashboard response", "item": "savingsAPI.accounts/AccountDashboard"]], "2.0": ["flow": "com.temenos.responder.flows.VersionInformationFlow"]]], "POST": ["routeTo": ["1.0": ["flow": "com.temenos.responder.flows.AdditionFlow"], "2.0": ["flow": "com.temenos.responder.flows.VersionInformationFlow", "request": ["description": "Account Dashboard request", "item": "savingsAPI.accounts/AccountDashboard"]]]]]
+            exception                | description     | methodName | versionName | directives
+            MethodNotFoundException  | 'method POST'   | "POST"     | "default"   | ["GET": ["routeTo": ["default": ["flow": "com.temenos.responder.flows.AdditionFlow"]]]]
+            MethodNotFoundException  | 'method DELETE' | "DELETE"   | "2.0"       | ["GET": ["routeTo": ["1.0": ["flow": "com.temenos.responder.flows.AdditionFlow"], "2.0": ["flow": "com.temenos.responder.flows.VersionInformationFlow"]]], "POST": ["routeTo": ["1.0": ["flow": "com.temenos.responder.flows.AdditionFlow"], "2.0": ["flow": "com.temenos.responder.flows.VersionInformationFlow"]]]]
+            MethodNotFoundException  | 'method PATCH'  | "PATCH"    | "2.0"       | ["GET": ["routeTo": ["1.0": ["flow": "com.temenos.responder.flows.AdditionFlow", "response": ["description": "Account Dashboard response", "item": "savingsAPI.accounts/AccountDashboard"]], "2.0": ["flow": "com.temenos.responder.flows.VersionInformationFlow"]]], "POST": ["routeTo": ["1.0": ["flow": "com.temenos.responder.flows.AdditionFlow"], "2.0": ["flow": "com.temenos.responder.flows.VersionInformationFlow", "request": ["description": "Account Dashboard request", "item": "savingsAPI.accounts/AccountDashboard"]]]]]
+            VersionNotFoundException | 'version 1.0'   | "GET"      | "1.0"       | ["GET": ["routeTo": ["default": ["flow": "com.temenos.responder.flows.AdditionFlow"]]]]
+            VersionNotFoundException | 'version 3.0'   | "GET"      | "3.0"       | ["GET": ["routeTo": ["1.0": ["flow": "com.temenos.responder.flows.AdditionFlow"], "2.0": ["flow": "com.temenos.responder.flows.VersionInformationFlow"]]], "POST": ["routeTo": ["1.0": ["flow": "com.temenos.responder.flows.AdditionFlow"], "2.0": ["flow": "com.temenos.responder.flows.VersionInformationFlow"]]]]
+            VersionNotFoundException | 'version 4.0'   | "POST"     | "4.0"       | ["GET": ["routeTo": ["1.0": ["flow": "com.temenos.responder.flows.AdditionFlow", "response": ["description": "Account Dashboard response", "item": "savingsAPI.accounts/AccountDashboard"]], "2.0": ["flow": "com.temenos.responder.flows.VersionInformationFlow"]]], "POST": ["routeTo": ["1.0": ["flow": "com.temenos.responder.flows.AdditionFlow"], "2.0": ["flow": "com.temenos.responder.flows.VersionInformationFlow", "request": ["description": "Account Dashboard request", "item": "savingsAPI.accounts/AccountDashboard"]]]]]
     }
 }
