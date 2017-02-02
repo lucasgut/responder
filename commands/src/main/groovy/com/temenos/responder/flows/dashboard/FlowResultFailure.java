3fd0dd1896bc2d048a01eacba5b69196ff5f74ff
Lucas Gut <lucaschueko@yahoo.es>
2017-02-02 15:16:32 +0000

    please do not merge

--- commands/src/main/groovy/com/temenos/responder/flows/dashboard/FlowResultFailure.java
+++
@@ -1,9 +0,0 @@
-package com.temenos.responder.flows.dashboard;
-
-import com.temenos.responder.context.FlowResult;
-
-public class FlowResultFailure extends FlowResult {
-    public FlowResultFailure(int status, String errorMessage) {
-        super(null, null, status, errorMessage);
-    }
-}
